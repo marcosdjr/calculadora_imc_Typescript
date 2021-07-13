00655c8004e489f5d8bd448f94ecb93c5b3c1463
Marcos Domingues Junior <marcos_domingues@hotmail.com>
2021-07-13 18:48:39 -0300

    Calculadora de IMC em TypeScript

---
+++ pages/index.tsx
@@ -0,0 +1,15 @@
+import ImcHeader from '../components/ImcHeader';
+import ImcCalculator from '../components/ImcCalculator';
+
+
+
+export default function Home() {
+  return (
+    <>
+    <div className="global">   
+    <ImcHeader />
+    <ImcCalculator />  
+    </div>
+    </>
+    )
+}
